bfacbb16cdd66d601c53239c19b5b5135d7769dd
hustacer <xiaobinly@gmail.com>
2019-06-24 18:05:54 +0900

    add fling to left or right

--- GestureDetector_Demo/app/src/main/java/com/example/com/gesturedetector_demo/MainActivity.java
+++ GestureDetector_Demo/app/src/main/java/com/example/com/gesturedetector_demo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.com.gesturedetector_demo;
 
+import android.support.constraint.solver.Cache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
@@ -18,7 +19,11 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textview);
-        mGestureDetector = new GestureDetector(new gestureListener());
+//        mGestureDetector = new GestureDetector(new gestureListener());
+//        mGestureDetector.setOnDoubleTapListener(new doubleTapListener());
+
+        // use SimpleOnGestureListener
+        mGestureDetector = new GestureDetector(new simpleGestureListener());
 
         textView.setOnTouchListener(this);
         textView.setFocusable(true);
@@ -65,6 +70,55 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             Toast.makeText(MainActivity.this, "onFling", Toast.LENGTH_SHORT).show();
             return false;
+        }
+    }
+
+    private class doubleTapListener implements GestureDetector.OnDoubleTapListener{
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Toast.makeText(MainActivity.this, "onSingleTapConfirmed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Toast.makeText(MainActivity.this, "onDoubleTap", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Toast.makeText(MainActivity.this, "onDoubleTap", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    private class simpleGestureListener extends GestureDetector.SimpleOnGestureListener{
+        final int FLING_MIN_DISTANCE = 100, FLING_MIN_VELOCITY = 200;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Toast.makeText(MainActivity.this, "onDown", Toast.LENGTH_LONG).show();
+            return super.onDown(e);
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Toast.makeText(MainActivity.this, "onDoubleTap", Toast.LENGTH_LONG).show();
+            return super.onDoubleTap(e);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE &&
+                    Math.abs(velocityX) > FLING_MIN_VELOCITY ) {
+                Toast.makeText(MainActivity.this, "onFling To Left", Toast.LENGTH_LONG).show();
+            } else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE &&
+                    Math.abs(velocityX) > FLING_MIN_VELOCITY ) {
+                Toast.makeText(MainActivity.this, "onFling To Right", Toast.LENGTH_LONG).show();
+            }
+            return super.onFling(e1, e2, velocityX, velocityY);
         }
     }
 }
