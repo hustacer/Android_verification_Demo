3eb754a8d8e47d9e58f9c013f3677556d487d671
hustacer <xiaobinly@gmail.com>
2019-06-26 15:06:32 +0900

    scrollTo scrollBy demo

---
+++ ScrollTo_scrollBy/app/src/main/java/com/example/com/scrollto_scrollby/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.com.scrollto_scrollby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout layout;
+    private Button button1;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.layout);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.scrollTo(-100,-100);
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.scrollBy(-100, -100);
+            }
+        });
+    }
+}
